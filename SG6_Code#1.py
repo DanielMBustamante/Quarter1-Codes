fed3b2cc815ae4aa3280d5b713dccb42e8a9e7c0
DanielMBustamante <danieljohn.bustamante@smc.pshs.edu.ph>
2025-09-20 07:24:37 +0800

    Summing 3 Numbers
    This was programmed to calculate and display the sum of 3 numbers.

---
+++ SG6_Code#1.py
@@ -0,0 +1,10 @@
+# Program for summing three numbers
+
+# Declaration of variables
+n1 = float(input("Enter the first number: "))
+n2 = float(input("Enter the second number: "))
+n3 = float(input("Enter the third number: "))
+sum = n1 + n2 + n3
+
+# Display the result
+print(f"The sum of the three numbers is: {sum:.2f}")
